1e211a9860932d7c1212b79fd3b47d1f1e0248a6
Ayushchatterjeesource <ayushchatterjee9641@gmail.com>
2026-09-06 18:44:01 +0530

    Add getRoute function for routing logic

--- public/router.js
+++ public/router.js
@@ -1,1 +1,42 @@
+export function getRoute() {
 
+    const hash = window.location.hash || "#/";
+
+    const cleanHash = hash.replace("#", "");
+
+    if (
+        cleanHash === "" ||
+        cleanHash === "/"
+    ) {
+        return {
+            page: "home"
+        };
+    }
+
+    if (cleanHash === "/products") {
+        return {
+            page: "products"
+        };
+    }
+
+    if (cleanHash === "/cart") {
+        return {
+            page: "cart"
+        };
+    }
+
+    if (cleanHash.startsWith("/product/")) {
+
+        const id =
+            cleanHash.split("/")[2];
+
+        return {
+            page: "product",
+            id: Number(id)
+        };
+    }
+
+    return {
+        page: "home"
+    };
+}
